67f86caffc0c43f85047fe59e81eec6f49e11ea9
Moemad-711 <57157211+Moemad-711@users.noreply.github.com>
2021-05-30 16:03:46 +0200

    add project files

---
+++ Compiler_Design_Prolog.pl
@@ -0,0 +1,221 @@
+% Auhtors: 
+%   1- Mohammed Emadeldin Mahmoud Radwan Mohammed El-Seoudi (Sec: 3)
+%   2- Mariam Mohammed Ibrahim (Sec: 4)
+%   3- Mohammed Aly Ahmed Aly (Sec: 3)
+%   4- Mahmoud Mamdouh Gaber (Sec: 4)
+%   5- Nada Magdy Zaki (Sec: 4)
+
+
+% Read input Tokenizing 
+parse_code_from_file:-
+	%%%%%%%%%%%%%%% Edit Source file Path %%%%%%%%%%%%%%%
+ 	read_file_to_string('D:\\4th Computer\\Compiler Design\\test.txt',InputString,[]),
+ 	string_tokenizer(InputString,TokenizedList),
+	maplist(atom_string, Tree, TokenizedList),
+	%%%%%%%%%%%%%%% Edit Output File Path %%%%%%%%%%%%%%%
+	parse_and_write('D:\\4th Computer\\Compiler Design\\output.txt',Tree).
+
+string_tokenizer(Input,Output):-
+	normalize_space(atom(S0), Input), % remove spaces in start and end of string %
+	change_character_in_string("+"," + ",S0,S1),
+	change_character_in_string("-"," - ",S1,S2),
+	change_character_in_string("*"," * ",S2,S3),
+	change_character_in_string("/"," / ",S3,S4),
+	change_character_in_string("{"," { ",S4,S5),
+	change_character_in_string("}"," } ",S5,S6),
+	change_character_in_string("("," ( ",S6,S7),
+	change_character_in_string(")"," ) ",S7,S8),
+	change_character_in_string(";"," ; ",S8,S9),
+	change_character_in_string("|"," | ",S9,S10),
+	change_character_in_string("&"," & ",S10,S11),
+	change_character_in_string("="," = ",S11,S12),
+	change_character_in_string("<"," < ",S12,S13),
+	change_character_in_string(">"," > ",S13,S14),
+    change_character_in_string("!"," ! ",S14,S15),
+	normalize_space(atom(SF), S15), %make_all_space_one space only Normalize white space in In. All leading and trailing white space is removed. All non-empty sequences for Unicode white space characters are replaced by a single space (\u0020) character%
+	split_string(SF, " ", "",Output).
+
+change_character_in_string(Old_Char,New_Char,String,New_String):-
+	atomic_list_concat(Atoms,Old_Char, String),
+	atomic_list_concat(Atoms,New_Char, New_String).
+
+%convert
+convert([], []). % there are no elements in list Xs
+convert([H|Ts], [Elm|Es]) :- % This is meant to be executed if there are elements in the list.
+   string_to_atom(H, Elm), % Built in function from [here][1].
+   convert(Ts, Es). % The recursive call to the tail T
+
+%%%%%%%%%%%%%%%%% Parse and Write %%%%%%%%%%%%%%%%%%%%%%%
+parse_and_write(OutputFile,Input):- 
+	phrase(parser(Tree),Input),
+	open(OutputFile,write,OutputStream),
+	write(Tree),
+	write_new_lang(OutputStream, Tree),
+	close(OutputStream).	%write_to_file(Tree).
+
+parse_and_write(OutputFile):- 
+	phrase(parser(Tree), [while,'(',x,<,20,')','{',x,=,x,+,1,;,'}']),
+	%phrase(parser(Tree),[x,=,x,+,1,;]),
+	%write(Tree).
+	open(OutputFile,write,OutputStream),
+	write_new_lang(OutputStream, Tree),
+	close(OutputStream).
+
+%%% Write Statment %%%
+write_new_lang(Stream, statment(Tree)):-
+	%write('hi stmt'), nl,
+	%write(Tree), nl,
+	write_new_lang(Stream, Tree).
+
+%%% Write Statments %%%
+write_new_lang(Stream, statments(Stmt, Stmts)):-
+	%rite('hi stmts'),
+	write(Stream, '    '),
+	write_new_lang(Stream, Stmt),
+	write_new_lang(Stream, Stmts),
+	nl(Stream).
+
+write_new_lang(Stream, statments([])):-
+	nl(Stream).
+
+%%% Write Assign_Stmt %%%
+write_new_lang(Stream, assign_stmt(ID, Equal, Expr)):-
+	write(Stream, ID),
+	write(Stream, Equal),
+	write_new_lang(Stream, Expr),
+	nl(Stream).
+
+%%% Write While_Stmt %%%
+write_new_lang(Stream, while(Cond, Stmt)):-
+	write(Stream, 'while '),
+	write_new_lang(Stream, Cond),
+	write(Stream, ':'),
+	nl(Stream),
+	write_new_lang(Stream, Stmt).
+
+%%% Write Condition %%%
+write_new_lang(Stream, conditions(Cond1, Op1, _, Cond2)):-
+	write_new_lang(Stream, Cond1),
+	( memberchk(Op1, ['&']), write(Stream, 'and ') | memberchk(Op1, ['|']), write(Stream, 'or ')),
+	write_new_lang(Stream, Cond2),
+	write(Stream, ' ').
+
+write_new_lang(Stream, conditions(Cond1)):-
+	write_new_lang(Stream, Cond1),
+	write(Stream, ' ').
+
+%%% Write Condition %%%
+write_new_lang(Stream, condition(Expr1, Op1, Op2, Expr2)):-
+	write_new_lang(Stream, Expr1),
+	write(Stream, Op1),
+	write(Stream, Op2),
+	write_new_lang(Stream, Expr2),
+	write(Stream, ' ').
+
+write_new_lang(Stream, condition(Expr1, Op1, Expr2)):-
+	write_new_lang(Stream, Expr1),
+	write(Stream, Op1),
+	write_new_lang(Stream, Expr2),
+	write(Stream, ' ').
+
+
+%%% Write Expression %%%
+write_new_lang(Stream, expression(Term, Op, Expr)):-
+	write_new_lang(Stream, Term),
+	write(Stream, Op),
+	write_new_lang(Stream, Expr).
+
+write_new_lang(Stream, expression(Bracket1, Term, Bracket2)):-
+	write(Stream, Bracket1),
+	write_new_lang(Stream, Term),
+	write(Stream, Bracket2).
+
+write_new_lang(Stream, expression(Term)):-
+	write_new_lang(Stream, Term).
+
+%%% Write Term %%%
+write_new_lang(Stream, term(Factor, Op, Term)):-
+	write_new_lang(Stream, Factor),
+	write(Stream, Op),
+	write_new_lang(Stream, Term).
+
+write_new_lang(Stream, term(Factor)):-
+	write_new_lang(Stream, Factor).
+
+%%% Write Factor %%%
+write_new_lang(Stream, factor(Num)):-
+	write(Stream, Num).
+
+write_new_lang(Stream, factor(Bracket1,Expr,Bracket2)):-
+	write(Stream, Bracket1),
+	write_new_lang(Stream, Expr),
+	write(Stream, Bracket2).
+
+
+
+%%%%%%%%%%%%%% Tools %%%%%%%%%%%%%%%%
+writelist(H|L):-
+	write(H), writelist(L).
+
+flatten([], []).
+flatten([H|T], R) :-
+    (
+       flatten(H, FH)
+    -> append(FH, FT, R)
+    ;  R = [H|FT]
+    ),
+    flatten(T, FT).
+
+
+%%%%%%%%%%%%%%%%%%% CFG Parsing Rules %%%%%%%%%%%%%%%%%%%%%%%
+parser([]) --> [].
+parser(Tree) --> stmt(Tree).
+
+%%%%%%%%%% Statment Rules %%%%%%%%%%%%
+stmt(statment(Tree)) --> while_stmt(Tree).
+stmt(statment(Tree)) --> assign_stmt(Tree).
+stmt(statment(Tree)) --> ['{'], stmts(Tree), ['}'].
+
+%%%%%%%%%% Statments Rules %%%%%%%%%%%%
+stmts(statments([])) --> [].
+stmts(statments(Stmt, Stmts)) --> stmt(Stmt), stmts(Stmts).
+
+%%%%%%%%%% While Rules %%%%%%%%%%%%
+while_stmt(while(Conds, Stmt)) --> ['while'], ['('], conditions(Conds), [')'], stmt(Stmt).
+
+%%%%%%%%%% Assignment Rules %%%%%%%%%%%%
+assign_stmt(assign_stmt(ID, '=', Expr)) --> id(ID), ['='], expression(Expr), [';'].
+
+%%%%%%%%%% Condition Rules %%%%%%%%%%%%
+conditions(conditions(Cond, Op1, Op2, Conds)) --> condition(Cond), log_op(Op1), log_op(Op2), conditions(Conds).
+conditions(conditions(Cond)) --> condition(Cond).
+
+%%%%%%%%%% Condition Rules %%%%%%%%%%%%
+condition(condition(Expr1, Op1, Expr2)) --> expression(Expr1), rel_op1(Op1), expression(Expr2).
+condition(condition(Expr1, Op1,Op2, Expr2)) --> expression(Expr1), rel_op1(Op1), rel_op2(Op2), expression(Expr2).
+
+%%%%%%%%%% Expression Rules %%%%%%%%%%%%
+expression(expression(Term, Op, Expr)) --> term(Term), add_sub_op(Op), expression(Expr).
+expression(expression('(', Term, ')')) --> ['('], term(Term), [')'].
+expression(expression(Term)) --> term(Term).
+
+%%%%%%%%%% Term Rules %%%%%%%%%%%%
+term(term(Factor, Op, Term)) --> factor(Factor), mul_div_op(Op), term(Term).
+term(term(Factor)) --> factor(Factor).
+
+%%%%%%%%%% Factor Rules %%%%%%%%%%%%
+factor(factor(Num)) --> num(Num).
+factor(factor(ID)) --> id(ID).
+factor(factor('(', Expr, ')')) --> ['('], expression(Expr), [')'].
+
+%%%%%%%%%% Terminals Rules %%%%%%%%%%%%
+num(Num) --> [Num], {number(Num)}.
+id(ID) --> [ID], {atom(ID)}.
+
+rel_op1(Op) --> [Op] , { memberchk(Op, ['>', '<', '!', '='])}.
+rel_op2(Op) --> [Op] , { memberchk(Op, ['='])}.
+
+log_op(Op) --> [Op] , { memberchk(Op, ['|', '&']) }.
+
+add_sub_op(Op) --> [Op], {memberchk(Op, ['+', '-'])}.
+mul_div_op(Op) --> [Op], {memberchk(Op, ['*', '/'])}. %memberchk on varible on list of elements if match one of them return true or false %
